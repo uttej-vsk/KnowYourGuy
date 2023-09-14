2511a8b6e7ac9aeae253f50e0398ec03b5982740
Uttej Venkata Sastry <uttejvsk@hotmail.com>
2023-09-14 14:34:13 +0530

    Navbar and homepage is done along with fetching data on homepage

--- app/page.jsx
+++ app/page.jsx
@@ -10,10 +10,39 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { endpoint } from '@/utils/endpoint'
 
+async function getAllCharacters() {
+  const data = await fetch(`${endpoint}/characters`)
+
+  if (!data.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return data.json()
+}
+
 export default async function Page() {
+  const data = await getAllCharacters()
+
   return (
-    <main className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl">hello from boilerplate!</h1>
+    <main>
+      <Container className="grid grid-cols-3 gap-3 py-5">
+        {data?.characters?.map(item => {
+          return (
+            <Link
+              href={`/characters/${item.slug}`}
+              key={item.id}
+              className="overflow-hidden rounded-lg"
+            >
+              <Image
+                src={item.avatar}
+                alt=""
+                className="transition-all duration-500 hover:scale-125 "
+                width={500}
+                height={200}
+              />
+            </Link>
+          )
+        })}
+      </Container>
     </main>
   )
 }
--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -10,5 +10,21 @@ import Image from 'next/image'
 import { TbArrowBigRightFilled } from 'react-icons/tb'
 
 export const Navigation = () => {
-  return <nav>navigation component</nav>
+  return (
+    <nav className="sticky top-0 backdrop-blur-xl bg-[rgba(0,0,0,0.8)] border-b border-slate-800 z-50">
+      <Container className="flex justify-between py-5">
+        <Link href="/">
+          <Image src="/logo.png" alt="Family Guy" width={70} height={50} />
+        </Link>
+
+        <Link
+          href="/quiz"
+          className="flex items-center gap-3 px-3 font-semibold text-black transition-colors bg-green-500 rounded-md duration-500 hover:bg-green-600"
+        >
+          <TbArrowBigRightFilled className="text-lg" />
+          Know Your Guy
+        </Link>
+      </Container>
+    </nav>
+  )
 }
